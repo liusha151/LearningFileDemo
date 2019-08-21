a75672d62ba71cf3a2a831a733d74702607aef67
huang wei <huangwei_smile@126.com>
2019-08-21 18:15:55 +0800

    update DataInputStreamDemo

--- src/main/java/filedemo/datastream/demo/DataInputStreamDemo.java
+++ src/main/java/filedemo/datastream/demo/DataInputStreamDemo.java
@@ -7,21 +7,21 @@ public class DataInputStreamDemo {
     public static void main(String[] args) throws Exception {
         DataInputStream dis = new DataInputStream(new FileInputStream("G:/appdev/o.txt"));
 
-        //int age = dis.readInt();
-        //long readLong = dis.readLong();
-        //float readFloat = dis.readFloat();
-
-        //double readDouble = dis.readDouble();
-
-        //boolean readBoolean = dis.readBoolean();
-
-        //String readUTF = dis.readUTF();
-
-        String name = dis.readUTF();
         int age = dis.readInt();
-        System.out.println(name);
-        System.out.println(age);
-        Integer.parseInt("1");
+        long readLong = dis.readLong();
+        float readFloat = dis.readFloat();
+
+        double readDouble = dis.readDouble();
+
+        boolean readBoolean = dis.readBoolean();
+
+        String readUTF = dis.readUTF();
+
+//        String name = dis.readUTF();
+//        int age = dis.readInt();
+//        System.out.println(name);
+//        System.out.println(age);
+//        Integer.parseInt("1");
     }
 
 }
